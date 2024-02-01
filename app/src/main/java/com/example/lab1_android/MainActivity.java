9d3b534e022de329811e43e6825d374d3702d2b0
Indiana <atwoodi26@up.edu>
2024-02-01 12:35:42 -0800

    you betta

--- app/src/main/java/com/example/lab1_android/MainActivity.java
+++ app/src/main/java/com/example/lab1_android/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.lab1_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // referencing code that I downloaded from lecture
+        setContentView(R.layout.more_complex);
+
+        TextView myFavTextView = new TextView(this);
+        myFavTextView.setText("some text");
+    }
+
+    /**
+    * changeMax (PART OF CODE FROM LECTURE)
+    *
+    * is called whenever the user taps the Update Count Button
+    * It increments the max count by 1.
+    */
+    public void changeMax(View v) {
+        // find the text view that contains the max number
+        TextView maxTextView = findViewById(R.id.textViewMaxValue);
+
+        // retrieve the value (a string)
+        CharSequence sequence = maxTextView.getText();
+        String textVal = sequence.toString();
+
+        // convert the String to an integer
+        int intVal = Integer.parseInt(textVal);
+
+        // increment integer
+        intVal++;
+
+        // convert back to String, put it back into the text view
+        String newTextVal = "" + intVal;
+        maxTextView.setText(newTextVal);
     }
 }
